37ade910015e4ec7797dbdc4483797de00d701a4
ligulfzhou <ligulfzhou53@gmail.com>
2023-09-30 11:34:44 +0800

    update customer

--- components/customer/EditCustomer.tsx
+++ components/customer/EditCustomer.tsx
@@ -1,12 +1,16 @@
-import React, {FC, useState} from "react";
-import {Modal, Form, Input} from "antd";
+import React, {FC, useEffect, useState} from "react";
+import {Modal, Form, Input, message} from "antd";
 import {Customer} from "@/types";
+import {UpdateOrderParam} from "@/requests/order";
+import moment from "moment/moment";
+import {updateCustomer, UpdateCustomerParam} from "@/requests";
+import useSWRMutation from "swr/mutation";
 
 
 interface Props {
     open: boolean,
-    closeFn: (success: boolean)=> void,
-    customer: Customer|undefined
+    closeFn: (success: boolean) => void,
+    customer: Customer | undefined
 }
 
 const EditCustomerModal: FC<Props> = (
@@ -19,46 +23,64 @@ const EditCustomerModal: FC<Props> = (
     const [savePRError, setSavePRError] = useState<string>('')
     const [form] = Form.useForm();
 
-    const onFinish = (values: any) => {
-        setSavePRError('')
-        form.resetFields()
+    const onFinish = (values: UpdateCustomerParam) => {
+        if (!customer?.id){
+            return
+        }
+
+        values['id'] = customer?.id
+        callUpdateCustomerAPI(values).then((res)=> {
+            if (res.code==0) {
+                message.success("修改成功")
+                closeFn(true)
+                form.resetFields()
+            } else {
+                message.error(res.msg)
+            }
+        })
     };
 
-    const onFinishFailed = (errorInfo: any) => {
-        console.log('Failed:', errorInfo);
-    };
+
+    const [formValues, setFormValues] = useState<UpdateCustomerParam | undefined>(undefined)
+
+    useEffect(() => {
+        let _formValues: UpdateCustomerParam = {
+            id: 0,
+            customer_no: customer?.customer_no || '',
+            notes: customer?.notes || ''
+        }
+        setFormValues(_formValues)
+        form.setFieldsValue(_formValues)
+    }, [customer])
+
+    const {
+        trigger: callUpdateCustomerAPI,
+        isMutating: callingUpdateCustomerAPI
+    } = useSWRMutation("/api/customer/update", updateCustomer)
 
     return (
         <div>
             <Modal
                 open={open}
-                title='Add PR'
+                title='修改客户'
                 onCancel={(e) => {
                     e.preventDefault()
                     form.resetFields()
                     setSavePRError('')
-                    closeFn(true)
+                    closeFn(false)
                 }}
+                onOk={() => form.submit()}
                 closable={true}
-                footer={
-                    null
-                }
+                confirmLoading={callingUpdateCustomerAPI}
             >
-                {savePRError ? (
-                    <div className='text-center text-red-600 font-bold text-pink-darker mb-2'>
-                        Error: {savePRError}
-                    </div>
-                ) : null}
-
                 <Form
                     form={form}
-                    name="修改客户"
-                    labelCol={{ span: 8 }}
-                    wrapperCol={{ span: 16 }}
-                    style={{ maxWidth: 600 }}
-                    initialValues={{ customer }}
+                    name="basic"
+                    labelCol={{span: 8}}
+                    wrapperCol={{span: 16}}
+                    style={{maxWidth: 600}}
+                    initialValues={{formValues}}
                     onFinish={onFinish}
-                    onFinishFailed={onFinishFailed}
                 >
                     <Form.Item
                         label="客户编号"
--- requests/customer.ts
+++ requests/customer.ts
@@ -2,10 +2,18 @@ import {Customer} from '@/types'
 import {commonDataPost} from "@/requests/common";
 
 
-export async function addCustomer(url: string, { arg }: { arg: Customer}) {
+
+
+export interface UpdateCustomerParam {
+    id: number,
+    customer_no: string,
+    notes: string
+}
+
+export async function addCustomer(url: string, { arg }: { arg: UpdateCustomerParam}) {
     return commonDataPost<Customer>(url, arg)
 }
 
-export async function updateCustomer(url: string, {arg}: {arg: Customer}) {
+export async function updateCustomer(url: string, {arg}: {arg: UpdateCustomerParam}) {
     return commonDataPost<Customer>(url, arg)
 }
