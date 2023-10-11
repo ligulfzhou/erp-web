307b4bd6d26cc6d75de0314703e4925a54f3b96c
ligulfzhou <ligulfzhou53@gmail.com>
2023-10-11 09:56:57 +0800

    import excel button

--- components/uploader/ExcelImporter.tsx
+++ components/uploader/ExcelImporter.tsx
@@ -27,12 +27,19 @@ const ExcelImporter: FC<Props> = (
                 message.loading({content: "正在上传excel..", key})
                 console.log(info.file, info.fileList);
             }
+
             if (info.file.status === 'done') {
-                console.log(info.file.response)
-                message.success({
-                    content: '导入成功', key
-                });
-                callback()
+                let res = info.file.response as EmptyResponse;
+                if (res.code == 0) {
+                    message.success({
+                        content: '导入成功', key
+                    });
+                    callback()
+                } else {
+                    message.error({
+                        content: `导入失败: ${res.msg}`, key
+                    });
+                }
             } else if (info.file.status === 'error') {
                 let res = info.file.response as EmptyResponse;
                 message.error({
